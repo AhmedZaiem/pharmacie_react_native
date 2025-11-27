42c055a8c68ddae92eb1a85e7a478fb2a7639384
AhmedZaiem <zaiemahmed30@gmail.com>
2025-11-27 20:04:25 +0100

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { usePatientStore } from "../store/patientStore";
+import { useAuthStore } from "../store/authStore";
 import AuthNavigator from "./AuthNavigator";
 import PatientNavigator from "./PatientNavigator";
 import PharmacienNavigator from "./PharmacienNavigator";
 
 export default function AppNavigator() {
-    const user = usePatientStore((state) => state.user);
+    const user = useAuthStore((state) => state.user);
 
     return (
         <NavigationContainer>
